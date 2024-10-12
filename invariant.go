09bca93e3e1b08bf278f8e648570c4d023fe668c
Maxim Motyshen <m.motyshen@gmail.com>
2024-10-12 21:28:33 +0300

    update

--- invariant.go
+++ invariant.go
@@ -6,32 +6,20 @@ import (
 )
 
 var (
-	ErrInvariantNotHolding = errors.New("invariant does not hold up")
+	ErrInvariantNotHolding = errors.New("invariant not satisfied")
 )
 
-type Invariant[T any] struct {
-	Name  string
-	Check func(T) error
-}
+type Invariant[T any] func(T) error
 
 func checkValInvariants[T any](val T, invariants []Invariant[T]) error {
-	for i, inv := range invariants {
-		err := inv.Check(val)
+	for _, inv := range invariants {
+		err := inv(val)
 		if err == nil { // all fine
 			continue
 		}
 
-		invName := inv.Name
-		if invName == "" {
-			invName = fmt.Sprintf("#%d", i)
-		}
-
-		return getInvariantError(invName, err)
+		return fmt.Errorf("%w: %w", ErrInvariantNotHolding, err)
 	}
 
 	return nil
-}
-
-func getInvariantError(invariantName string, err error) error {
-	return fmt.Errorf("%w: invariant '%s' fails with error: %w", ErrInvariantNotHolding, invariantName, err)
 }
